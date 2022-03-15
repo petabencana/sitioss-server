9a4159fe827f379bef3613564ca80be50c5eeb2b
ruthvik129 <39596102+ruthvik129@users.noreply.github.com>
2022-03-15 11:58:22 +0530

    Made changes to for join to fetch partner icon

--- src/api/routes/reports/model.js
+++ src/api/routes/reports/model.js
@@ -19,7 +19,7 @@ export default (config, db, logger) => ({
       let query = `SELECT pkey, created_at, source,
       status, url, image_url, disaster_type, report_data, tags, title, text,
       the_geom, ${config.TABLE_COGNICITY_PARTNERS}.partner_code ,${config.TABLE_COGNICITY_PARTNERS}.partner_icon FROM ${config.TABLE_REPORTS}
-      INNER JOIN ${config.TABLE_COGNICITY_PARTNERS} ON ${config.TABLE_REPORTS}.partner_code=${config.TABLE_COGNICITY_PARTNERS}.partner_code
+      LEFT JOIN ${config.TABLE_COGNICITY_PARTNERS} ON ${config.TABLE_REPORTS}.partner_code=${config.TABLE_COGNICITY_PARTNERS}.partner_code
       WHERE ((disaster_type = 'flood' AND created_at >= to_timestamp($1)) 
       OR (disaster_type = 'earthquake' AND created_at >= to_timestamp($2))
       OR (disaster_type = 'wind' AND created_at >= to_timestamp($3))
