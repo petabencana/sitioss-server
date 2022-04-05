e7e8f7bb642f7f907e3b4d8cfcdad167d04aa8d7
ruthvik129 <ruthvik129@gmail.com>
2022-04-05 15:40:14 +0530

    Handled corrupt png images while creating partners

--- src/api/routes/partners/index.js
+++ src/api/routes/partners/index.js
@@ -49,6 +49,7 @@ export default ({ config, db, logger }) => {
       contentType: function (req, file, cb) {
         cb(null, file.mimetype);
       },
+      contentEncoding: "base64",
     }),
   });
 
